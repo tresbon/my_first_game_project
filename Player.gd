91222a98ed7ca011973241c1e51f98575872be67
tresbon <vladi.zubkov@gmail.com>
2019-09-28 23:40:16 +0300

    Added scenes: Main, Coin

--- Player.gd
+++ Player.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal pickup
+signal hurt
 export (int) var speed
 var velocity = Vector2()
 var screensize = Vector2(480,720)
@@ -17,10 +19,33 @@ func get_input():
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-
+		
 func _process(delta):
 	get_input()
 	
 	position += velocity * delta 
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
+
+	if velocity.length() > 0:
+		$AnimatedSprite.animation = 'run'
+		$AnimatedSprite.flip_h = velocity.x < 0
+	else:
+		$AnimatedSprite.animation = 'idle'
+		
+func start(pos):
+	set_process(true)
+	position = pos
+	$AnimatedSprite.animation = 'idle'
+	
+func die():
+	$AnimatedSprite.animation = 'hurt'
+	set_process(false)
+
+func _on_Player_area_entered(area):
+	if area.is_in_group('coins'):
+		area.pickup()
+		emit_signal('pickup')
+	if area.is_in_group('obstacles'):
+		emit_signal('hurt')
+		die()
