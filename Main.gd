a72088aa132cd79ed18ac43367b4eeb61fd46f01
tresbon <vladi.zubkov@gmail.com>
2019-09-29 02:56:19 +0300

    Added main menu and coins effect

--- Main.gd
+++ Main.gd
@@ -14,7 +14,6 @@ func _ready():
 	screensize = get_viewport().get_visible_rect().size
 	$Player.screensize = screensize
 	$Player.hide()
-	new_game()
 	
 func new_game():
 	playing = true
@@ -25,6 +24,8 @@ func new_game():
 	$Player.show()
 	$GameTimer.start()
 	spawn_coins()
+	$HUD.update_score(score)
+	$HUD.update_timer(timeleft)
 	
 func spawn_coins():
 	for i in range(4+level):
@@ -39,3 +40,24 @@ func _process(delta):
 		level += 1
 		timeleft += 5
 		spawn_coins()
+
+func _on_GameTimer_timeout():
+	timeleft -= 1
+	$HUD.update_timer(timeleft)
+	if timeleft <= 0:
+		game_over()
+
+func _on_Player_hurt():
+	game_over()
+
+func _on_Player_pickup():
+	score += 1
+	$HUD.update_score(score)
+
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	for coin in $CoinContainer.get_children():
+		coin.queue_free()
+	$HUD.show_game_over()
+	$Player.die()
